900207ecbb3209e255a2be90f532143c4d349633
Ami Zou <ami.yx.zou@gmail.com>
2017-12-05 13:34:40 -0500

    three methods

--- src/Prep/findDuplicates.java
+++ src/Prep/findDuplicates.java
@@ -7,8 +7,46 @@ import java.util.List;
 //Notice: range is 0 to N-1
 
 public class findDuplicates {
-	public int xorMethod(int[] arr){
-		return 0;
+	public void xorMethod(int[] arr, int size){
+		 /* Will hold xor of all elements */
+        int xor = arr[0];
+         
+        /* Will have only single set bit of xor */
+        int set_bit_no;
+         
+        int i;
+        int n = size - 2;
+        int x = 0, y = 0;
+ 
+        /* Get the xor of all elements in arr[] and {1, 2 .. n} */
+        for (i = 1; i < size; i++)
+            xor ^= arr[i];
+        for (i = 1; i <= n; i++)
+            xor ^= i;
+ 
+        /* Get the rightmost set bit in set_bit_no */
+        set_bit_no = (xor & ~(xor - 1));
+ 
+        /* Now divide elements in two sets by comparing rightmost set
+           bit of xor with bit at same position in each element. */
+        for (i = 0; i < size; i++) {
+            int a = arr[i] & set_bit_no;
+            if (a != 0)
+                x = x ^ arr[i]; /*XOR of first set in arr[] */
+            else
+                y = y ^ arr[i]; /*XOR of second set in arr[] */
+        }
+        
+        for (i = 1; i <= n; i++) {
+            int a = i & set_bit_no;
+            if (a != 0)
+                x = x ^ i; /*XOR of first set in arr[] and {1, 2, ...n }*/
+            else
+                y = y ^ i; /*XOR of second set in arr[] and {1, 2, ...n } */
+        }
+ 
+        System.out.println("The two reppeated elements are :");
+        System.out.println(x + " " + y);
 	}
 	
 	public List<Integer> restoreBasedOnMod(int[] arr, int n) { // n is != arr.length; one is size, but the other is range
@@ -36,14 +74,13 @@ public class findDuplicates {
 	}
 	
 	public void mathEquationForTwoDuplicates(int[] arr, int size) {
-/*
-Let summation of all numbers in array be S and product be P:
-
-X + Y = S – n(n+1)/2
-XY = P/n!		
-
-Then we can use Math.sqrt and other functions to calculate X and Y
- */
+	/*
+		Let summation of all numbers in array be S and product be P:
+		X + Y = S – n(n+1)/2
+		XY = P/n!		
+		Then we can use Math.sqrt and other functions to calculate X and Y
+	*/
+		
 		/* S is for sum of elements in arr[] */
         int S = 0;
          
